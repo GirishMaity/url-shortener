1d61c2eca79f3eb6bf264b5de27d5d8fd16a4d3d
Girish Maity <girishmaity164@gmail.com>
2024-02-18 22:57:13 +0530

    removed uri

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./config/config");
-
+require("dotenv").config();
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
 
-connectToMongoDB(
-  "mongodb+srv://girishmaity164:APxoI1nFWuctazca@urlshortener.ssfoivl.mongodb.net/?retryWrites=true&w=majority"
-).then(() => console.log("MongoDB connected"));
+connectToMongoDB(process.env.MONGO_URI).then(() =>
+  console.log("MongoDB connected")
+);
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
 
-app.listen(PORT, () => console.log(`SERVER LISTENING AT PORT ${PORT}`));
+app.listen(process.env.PORT || 8000, () =>
+  console.log(`SERVER LISTENING AT PORT ${process.env.PORT || 8000}`)
+);
